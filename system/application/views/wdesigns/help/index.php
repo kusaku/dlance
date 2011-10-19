7c28b8a36c0650c3b73abcdc4811e6666846656e
aleksander.z <aleksander.z@fabricasaitov.ru>
2011-10-19 18:50:54 +0400

    Styled wdesigns/help/index.php and view.php

--- system/application/views/wdesigns/help/index.php
+++ system/application/views/wdesigns/help/index.php
@@ -1,39 +1,25 @@
-<div id="yui-main">
-<div class="yui-b clearfix"> 
-
-<ul class="marketnav">
-<li class="lvl-1"><a href="/help/">Все разделы</a></li>
-<? foreach($help_categories as $row): ?> 
-
-<li class="lvl-1"><a href="/help/index/?category=<?=$row['id']?>"><?=$row['name']?></a></li>
-	<? foreach($help_pages as $row2): ?> 
-		<? if( $row['id'] == $row2['category'] ): ?>
-			<li class="lvl-2"><a href="/help/<?=$row2['id']?>.html"><?=$row2['title']?></a></li>
-		<? endif; ?>
+<div class="sideBar">
+	<div class="designsCategories">
+		<h3><a href="/help/">Все разделы</a></h3>
+		<ul>
+		<? foreach($categories as $row): ?> 
+			<li class="lvl-1"><a href="/help/index/?category=<?=$row['id']?>"><?=$row['name']?></a></li>
+		<? endforeach; ?>
+		</ul>
+	</div>
+</div>
+<div class="content">
+	<h3>Помощь</h3>
+	<a href="/help/">Все разделы</a>
+	<ul>
+	<? foreach($help_categories as $row): ?> 
+	<li class="lvl-1"><a href="/help/index/?category=<?=$row['id']?>"><?=$row['name']?></a><ul>
+		<? foreach($help_pages as $row2): ?> 
+			<? if( $row['id'] == $row2['category'] ): ?>
+				<li class="lvl-2"><a href="/help/<?=$row2['id']?>.html"><?=$row2['title']?></a></li>
+			<? endif; ?>
+		<? endforeach; ?>
+	</ul></li>
 	<? endforeach; ?>
-<? endforeach; ?>
-</ul>
-
-</div>
-</div>
-
-
-<div id="sidebar" class="yui-b">
-<div class="hd"></div>
-<div class="bd clearfix">
-
-<div class="sideblock">
-<ul class="marketnav">
-<h3><a href="/help/">Все разделы</a></h3>
-<li class="lvl-1"><a href="/help/">Все разделы</a></li>
-<? foreach($categories as $row): ?> 
-
-<li class="lvl-1"><a href="/help/index/?category=<?=$row['id']?>"><?=$row['name']?></a></li>
-
-<? endforeach; ?>
-</ul>
-</div>
-
-</div>
-<div class="ft"></div>
+	</ul>
 </div>
