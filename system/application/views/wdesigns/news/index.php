a41fc9a823da047ae768bc0e223a02db21d9b527
aleksander.z <aleksander.z@fabricasaitov.ru>
2011-10-19 15:26:41 +0400

    Styled wdesigns/news/index.php and view.php

--- system/application/views/wdesigns/news/index.php
+++ system/application/views/wdesigns/news/index.php
@@ -1,19 +1,10 @@
-<div id="yui-main">
-<div class="yui-b clearfix"> 
+<h2>Новости проекта</h2>
 
 <? if( !empty($news) ): ?>
 <? foreach($news as $row): ?>
-<div class="rnd mb10">
-      <div>
-        <div>
-          <div>
-            <div class="row-title"><a href="/news/<?=$row['id']?>.html"><?=$row['title']?></a> </div>
-            <div class="row-date"><?=$row['date']?></div>
-            <div class="row-desc clearfix skip-rating"><?=$row['descr']?></div>
-          </div>
-        </div>
-      </div>
-    </div>
+	<div class="newsTitle"><a href="/news/<?=$row['id']?>.html"><?=$row['title']?></a> </div>
+	<div class="newsDate"><?=$row['date']?></div>
+	<div class="newsDesc"><?=$row['descr']?></div>
 <? endforeach; ?>
 <?=$page_links?>
 <? else: ?>
@@ -21,24 +12,3 @@
 <? endif; ?>
 
 
-</div>
-</div>
-
-
-<div id="sidebar" class="yui-b">
-<div class="hd"></div>
-<div class="bd clearfix">
-
-<div class="sideblock">
-<h3>Новости проекта</h3>
-	<ul class="latest-page">
-<? foreach($newest_news as $row): ?>
-		<li><?=$row['date']?> | <a href="/news/<?=$row['id']?>.html"><?=$row['title']?></a></li>
-<? endforeach; ?>
-	</ul>
-
-</div>
-
-</div>
-<div class="ft"></div>
-</div>
--- system/application/views/wdesigns/news/view.php
+++ system/application/views/wdesigns/news/view.php
@@ -1,39 +1,17 @@
-<div class="yui-g">
-	<h1 class="title"><?=$title?></h1>
-	<div class="desc"><?=$descr?></div>
-	<noindex>
-	<div class="subtitle">Дата: <?=$date?></div>
-	</noindex>
-
-</div>
+<h2>Новости проекта</h2><br/>
+<h3><b><?=$title?></b></h3>
+<div class="newsQuote"><?=$descr?></div>
+<noindex>
+<div class="newsDate"><?=$date?></div>
+</noindex>
 
 
-<div id="yui-main">
-<div class="yui-b clearfix">
-
-<div class="content">
+<div class="newsContent">
 <?=nl2br($text)?>
 </div>
 
-</div>
-</div>
 
-
-
-<div id="sidebar" class="yui-b">
-<div class="hd"></div>
-<div class="bd clearfix">
-
-<div class="sideblock">
-<h3>Новости проекта</h3>
-	<ul class="latest-page">
+<h3>Другие новости проекта:</h3>
 <? foreach($newest_news as $row): ?>
-		<li><?=$row['date']?> | <a href="/news/<?=$row['id']?>.html"><?=$row['title']?></a></li>
+	<p><?=$row['date']?> | <a class="orange" href="/news/<?=$row['id']?>.html"><?=$row['title']?></a></p>
 <? endforeach; ?>
-	</ul>
-
-</div>
-
-</div>
-<div class="ft"></div>
-</div>
