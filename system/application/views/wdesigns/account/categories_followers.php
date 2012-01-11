cd0d6a89ca8d08469211cea29a3aff6d3ded5bac
aleksander.z <aleksander.z@fabricasaitov.ru>
2012-01-11 16:14:17 +0400

    updated wdesigns/account/categories_followers.php

--- system/application/views/wdesigns/account/categories_followers.php
+++ system/application/views/wdesigns/account/categories_followers.php
@@ -1,58 +1,40 @@
-<div id="yui-main">
-<div class="yui-b">
-
-<h1> <a href="/account/categories_followers">Подписки на рубрики</a> </h1>
-
-<p class="subtitle"> Подписки на рубрики </p>
-
-<?=validation_errors()?>
-
-
-
-
-
-<form action="" method="post" />
-<? if( !empty($categories) ): ?>
-<table class="services">
-<tbody>
-
-<? foreach($categories as $row): ?>
-
-<? if( $row['parent_id'] == 0): ?>
-<tr>
-<th style="width: 5px;"></th>
-<th class="txtl"><h4><?=$row['name']?></h4></th>
-</tr>
-<? endif; ?>
-
-	<? foreach($categories as $row2): ?>
-
-			<? if( $row['id'] == $row2['parent_id'] ): ?>
-<tr>
-<td>
-<input type="checkbox" name="category[]" value="<?=$row2['id']?>"<? if( !empty($select) ): if( in_array($row2['id'], $select) ): ?> checked="checked" <? endif; endif; ?> />
-</td>
-<td><?=$row2['name']?></td>
-</tr>
-			<? endif; ?>
-
-	<? endforeach; ?>
-
-<? endforeach; ?>
-
-</tbody>
-</table>
-<? else: ?>
-<p>Услуги не найдены.</p>
-<? endif; ?>
-<input name="submit" type="submit" value="Сохранить">
-</form>
-
-
-
-  </div>
-
-</div>
-<!--/yui-main-->
-
 <? $this->load->view('wdesigns/account/block'); ?>
+
+<div class="content">
+	<div class="userResponseHeader">
+		<div class="addResponse">
+			<div class="addResponseRightBrdr">
+				<a href="#">отменить все</a>
+				&nbsp;&nbsp;&nbsp;
+				<a href="#">снять отметки</a>
+			</div>
+		</div>
+		<h3>Рассылка по рубрикам:</h3>
+	</div>
+	<div class="contentWrapperBorderLeft" style="min-height:580px;">
+		<?=validation_errors()?>
+		<form action="" method="post" class="subscribeSettings">
+		<? if( !empty($categories) ): ?>
+			<? foreach($categories as $row): ?>
+				<? if( $row['parent_id'] == 0): ?>
+					<h4><?=$row['name']?></h4>
+				<? endif; ?>
+
+				<ul class="line">
+				<? foreach($categories as $row2): ?>
+					<? if( $row['id'] == $row2['parent_id'] ): ?>
+						<li>
+							<span class="niceCheck"><input type="checkbox" name="category[]" value="<?=$row2['id']?>"<? if( !empty($select) ): if( in_array($row2['id'], $select) ): ?> checked="checked" <? endif; endif; ?> /></span>
+							<label for="agree"><a href="#"><?=$row2['name']?></a></label>
+						</li>
+						<? endif; ?>
+				<? endforeach; ?>
+				</ul>
+			<? endforeach; ?>
+		<? else: ?>
+			<p>Ничего не найдено.</p>
+		<? endif; ?>
+		<input name="submit" type="submit" value="Сохранить">
+		</form>
+	</div>
+</div>
