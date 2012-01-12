a7119a2b11847c26c6cf203d9a3b2005ac5d6201
aleksander.z <aleksander.z@fabricasaitov.ru>
2012-01-12 18:35:46 +0400

    modifed wdesigns/account/purses.php

--- system/application/views/wdesigns/account/purses.php
+++ system/application/views/wdesigns/account/purses.php
@@ -1,39 +1,40 @@
-<div id="yui-main">
-<div class="yui-b">
-
-
-<h1><a href="/account/purses/">Кошельки</a></h1>
-<p class="subtitle"> Список ваших кошельков, для вывода. Для создания нового нажмите: "<a href="/account/purses_add/">Добавить кошелек</a>"</p>
-
-
-<? if( !empty($data) ): ?>
-<table class="offers">
-<tr>
-<th class="txtl" style="width:100px;">Номер кошелька</th>
-<th style="width:100px;">Сумма выплат</th>
-<th style="width:150px;">Дата создания</th>
-<th style="width:150px;">Последняя операция</th>
-<th></th>
-</tr>
-<? foreach($data as $row): ?>
-<tr>
-<td class="title"><?=$row['purse']?></td>
-<td class="budget txtc"><strong><?=$row['amount']?></strong> USD</td>
-<td class="owner txtc"><?=$row['date']?></td>
-<td class="owner txtc"><?=$row['last_operation']?></td>
-<td><span class="fr"><a href="/account/purses_del/<?=$row['id']?>">Удалить</a></span></td>
-</tr>
-<? endforeach; ?>
-</table>
-
-<? else: ?>
-<p>Кошельки отсутствуют.</p>
-<? endif; ?>
-
-
-  </div>
-
-</div>
-<!--/yui-main-->
-
 <? $this->load->view('wdesigns/account/block'); ?>
+
+<div class="content">
+	<div class="userMoneyHeader top">
+		<div class="addItem">
+			<div class="addItemRightBrdr">
+				<span>+</span> <a href="/account/purses_add/">добавить кошелек</a>
+			</div>
+		</div>
+		<h3>Мои кошельки:</h3>
+		<ul class="userMoneyCategory">
+			<li style="width:100px;"><a href="#" class="active">Номер кошелька</a></li>
+			<li style="width:100px;"><a href="#">Сумма выплат</a></li>
+			<li style="width:100px;"><a href="#">Дата создания</a></li>
+			<li style="width:120px;"><a href="#">Последняя операция</a></li>
+		</ul>
+	</div>
+	<div class="userMoneyList">
+		<? if( !empty($data) ): ?>
+		<ul class="worksList">
+			<? foreach($data as $row): ?>
+			<li>
+				<p class="ctype"><img src="images/yad.png" alt="Нужен тип кошелька" /></p>
+				<p class="nomber"><span><?=$row['purse']?></span></p>
+				<p class="summ"><?=$row['amount']?> USD</p>
+				<p class="date"><?=$row['date']?></p>
+				<p class="last"><?=$row['last_operation']?></p>
+				<p class="spec"><span>x</span> <a href="/account/purses_del/<?=$row['id']?>">удалить</a></p>
+			</li>
+			<? endforeach; ?>
+		</ul>
+		<? else: ?>
+			<ul class="worksList">
+				<li>
+					<p>Кошельки отсутствуют.</p>
+				</li>
+			</ul>
+		<? endif; ?>
+	</div>
+</div>
