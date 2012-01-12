f222cd5734c744ce940d2e4bf00c54d8a7db3142
aleksander.z <aleksander.z@fabricasaitov.ru>
2012-01-12 19:06:27 +0400

    modifed wdesigns/account/downloads.php

--- system/application/views/wdesigns/account/downloads.php
+++ system/application/views/wdesigns/account/downloads.php
@@ -1,31 +1,56 @@
-<div id="yui-main">
-<div class="yui-b">
-
-
-<h1><a href="/account/downloads">Загрузки</a></h1>
-
-<? if( !empty($data) ): ?>
-<table class="offers">
-<tr>
-<th class="txtl" style="width:100px;">Название</th>
-<th style="width:100px;">Осталось</th>
-<th></th>
-</tr>
-<? foreach($data as $row): ?>
-<tr>
-<td class="title"><?=$row['title']?></td>
-<td class="owner txtc"><?=$row['left_date']?></td>
-<td><span class="fr"><a href="/account/download/<?=$row['code']?>">Скачать</a></span></td>
-</tr>
-<? endforeach; ?>
-</table>
-
-<? else: ?>
-<p>Загрузки отсутствуют.</p>
-<? endif; ?>
-  </div>
-
-</div>
-<!--/yui-main-->
-
 <? $this->load->view('wdesigns/account/block'); ?>
+<div class="content">
+	<div class="cartResults">
+		<div class="userBasketHeader">
+			<h3>Получить заказ на оформленные дизайны:</h3>
+		</div>
+		<div class="contentWrapperBorderLeft">
+			<div class="searchResultsList">
+				<? if( !empty($data) ): ?>
+				<ul class="designsList">
+					<? foreach($data as $row): ?>
+					<li> 
+						<a href="<?=$row['full_image']?>" class="zoom" title="<?=$row['title']?>"><img src="<?=$row['small_image']?>" alt="<?=$row['title']?>"/></a>
+						<p><a href="/designs/<?=$row['id']?>.html"><?=$row['title']?></a></p>
+						<!-- Это откуда берется? -->
+						<p><?=$row['category']?><br/>
+						Исходник: <?=$row['source']?></p>
+						<p>Рейтинг: <span><?=$row['rating']?></span><br/>
+						Осталось: <span><?=$row['left_date']?></span><br/>
+						Цена: <span><?=$row['price_1']?> руб.</span></p>
+						<p class="details"><a href="/account/download/<?=$row['code']?>">Скачать</a></p>
+					</li>
+					<? endforeach; ?>
+				</ul>
+				<div class="paginationControl">
+					<?=$page_links?>
+				</div>
+				<!--
+				<div class="itemsOnPage">
+					<p>кол-во дизайнов на страницу:</p>
+					<ul class="pageList">
+					<? $results = array(10,20,50,100);
+						foreach ($results as $items){
+							if($items==$input['result']){
+								echo "<li class=\"active\">".$items."</li>";
+							}
+							else {
+								if (mb_strpos($_SERVER['QUERY_STRING'],"&result=")) {
+									echo "<li><a href=\"".mb_substr($_SERVER['QUERY_STRING'],0,(mb_strpos($_SERVER['QUERY_STRING'],"&result=")))."&result=".$items."\">".$items."</a></li>";
+								}
+								else {
+									echo "<li><a href=\"".$_SERVER['QUERY_STRING']."&result=".$items."\">".$items."</a></li>";
+								}
+							}
+						}
+					?>
+					</ul>
+				</div>
+				-->
+				<? else: ?>
+					<p>Загрузки отсутствуют.</p>
+				<? endif; ?>
+			</div>
+		</div>
+	</div>
+</div>
