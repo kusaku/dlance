35302398f2eeede9a4d20d8320b56b5c2fb911c1
aleksander.z <aleksander.z@fabricasaitov.ru>
2012-01-12 17:40:16 +0400

    modifed wdesigns/account/purchased.php

--- system/application/views/wdesigns/account/purchased.php
+++ system/application/views/wdesigns/account/purchased.php
@@ -1,59 +1,56 @@
-<div id="yui-main">
-<div class="yui-b">
-
-
-<h1><a href="/account/purchased">Купленные</a></h1>
-
-<? if( !empty($data) ): ?>
-<?=show_highslide()?>
-<table class="portfolio">
-<tr>
-<th style="width:150px;">Превью</th>
-<th>Название / Краткое описание / Ссылка</th>
-</tr>
-<? foreach($data as $row): ?>
-<tr>
-<td class="thumb" rowspan="3" style="width:150px;">
-<a href="<?=$row['full_image']?>" class="highslide" onclick="return hs.expand(this)">
-<img src="<?=$row['small_image']?>" title="<?=$row['title']?>" />
-</a>
-</td>
-<td class="info">Категория: <?=$row['category']?></td>
-</tr>
-
-<tr>
-<td>
-<h4><a href="/designs/<?=$row['design_id']?>.html"><?=$row['title']?></a></h4>
-<p><?=$row['text']?></p>
-
-<a href="/account/create_download/<?=$row['design_id']?>">Перейти к скачке</a> | <a href="/account/to_email/<?=$row['design_id']?>">Отправить на почту</a>
-</td>
-</tr>
-
-<tr>
-<td>
-Дата: <?=$row['date']?> |
-Продаж: <?=$row['sales']?> |
-<? if( $row['kind'] == 1 ): ?>
-Покупка
-<? else: ?>
-Выкуп
-<? endif; ?>
-
-
-</td>
-</tr>
-<? endforeach; ?>
-</table>
-<?=$page_links?>
-<? else: ?>
-<p>Дизайны отсутствуют.</p>
-<? endif; ?>
-
-
-  </div>
-
-</div>
-<!--/yui-main-->
-
 <? $this->load->view('wdesigns/account/block'); ?>
+<div class="content">
+	<div class="cartResults">
+		<div class="userBasketHeader">
+			<h3>Купленные:</h3>
+		</div>
+		<div class="contentWrapperBorderLeft">
+			<div class="searchResultsList">
+				<? if( !empty($data) ): ?>
+				<ul class="designsList">
+					<? foreach($data as $row): ?>
+					<li> 
+						<a href="<?=$row['full_image']?>" class="zoom" title="<?=$row['title']?>"><img src="<?=$row['small_image']?>" alt="<?=$row['title']?>"/></a>
+						<p><a href="/designs/<?=$row['id']?>.html"><?=$row['title']?></a></p>
+						<!-- Это откуда берется? -->
+						<p><?=$row['category']?><br/>
+						Исходник: <?=$row['source']?></p>
+						<p>Рейтинг: <span><?=$row['rating']?></span><br/>
+						Скачиваний: <span><?=$row['sales']?></span><br/>
+						Цена: <span><?=$row['price_1']?> руб.</span></p>
+						<p class="details"><a href="/account/create_download/<?=$row['design_id']?>">Перейти к скачке</a></p>
+					</li>
+					<? endforeach; ?>
+				</ul>
+				<div class="paginationControl">
+					<?=$page_links?>
+				</div>
+				<!--
+				<div class="itemsOnPage">
+					<p>кол-во дизайнов на страницу:</p>
+					<ul class="pageList">
+					<? $results = array(10,20,50,100);
+						foreach ($results as $items){
+							if($items==$input['result']){
+								echo "<li class=\"active\">".$items."</li>";
+							}
+							else {
+								if (mb_strpos($_SERVER['QUERY_STRING'],"&result=")) {
+									echo "<li><a href=\"".mb_substr($_SERVER['QUERY_STRING'],0,(mb_strpos($_SERVER['QUERY_STRING'],"&result=")))."&result=".$items."\">".$items."</a></li>";
+								}
+								else {
+									echo "<li><a href=\"".$_SERVER['QUERY_STRING']."&result=".$items."\">".$items."</a></li>";
+								}
+							}
+						}
+					?>
+					</ul>
+				</div>
+				-->
+				<? else: ?>
+					<p>Дизайны отсутствуют.</p>
+				<? endif; ?>
+			</div>
+		</div>
+	</div>
+</div>
