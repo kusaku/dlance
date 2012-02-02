e293376cd9b7f17012e07cd316d63c389fb587a0
Konstantin.I <konstantin.i@fabricasaitov.ru>
2012-02-02 14:14:00 +0400

    Robokassa

--- system/application/views/admin/settings.php
+++ system/application/views/admin/settings.php
@@ -68,12 +68,93 @@
 						<textarea name="description"><?=$description?></textarea>
 					</p>
 				</div>
-
+				
 				<div class="grid_16">
 					<p>
 						<label>Ключевые слова <small>Максимальное количество символов 10000.</small></label>
 						<textarea name="keywords"><?=$keywords?></textarea>
 					</p>
+				</div>
+				
+				<!-- ROBOX -->
+				<div class="grid_16">
+					<p>
+					<h2>RoboKassa</h2>
+					</p>
+				</div>
+				<div class="grid_8">
+					<p>
+						<label>Логин <small>Логин мерчанта</small></label>
+						<input name="pay_robox_login" value="<?=$pay_robox_login?>" />
+					</p>
+				</div>
+				<div class="grid_8">
+					<p>
+						<label>Режим <small>Режим взаимодействия с сервером</small></label>
+						<select name="pay_robox_mode">
+							<option value="0" <?if($pay_robox_mode == 0){?>selected<?}?>>Тестовый режим</option>
+							<option value="1" <?if($pay_robox_mode == 1){?>selected<?}?>>Боевой режим</option>
+						</select>
+					</p>
+				</div>
+				<div class="grid_8">
+					<p>
+						<label>Пароль 1 <small>Пароль мерчанта №1</small></label>
+						<input name="pay_robox_pass1" value="<?=$pay_robox_pass1?>" />
+					</p>
+				</div>
+				<div class="grid_8">
+					<p>
+						<label>Пароль 2 <small>Пароль мерчанта №2</small></label>
+						<input name="pay_robox_pass2" value="<?=$pay_robox_pass2?>" />
+					</p>
+				</div>
+				<!-- END ROBOX -->
+				
+				<!-- Qiwi -->
+				<div class="grid_16">
+					<p>
+					<h2>Настройки Qiwi</h2>
+					</p>
+				</div>
+				<div class="grid_5">
+					<p>
+						<label>ID <small>ID-номер партнера</small></label>
+						<input name="pay_qiwi_id" value="<?=$pay_qiwi_id?>" />
+					</p>
+				</div>
+				<div class="grid_5">
+					<p>
+						<label>LifeTime <small>Время жизни счета в часах</small></label>
+						<input name="pay_qiwi_lt" value="<?=$pay_qiwi_lt?>" />
+					</p>
+				</div>
+				<div class="grid_6">
+					<p>
+						<label>Только агенты <small>Проверка на наличие кошелька</small></label>
+						<select name="pay_qiwi_agt">
+							<option value="0" <?if($pay_qiwi_agt == 0){?>selected<?}?>>Нет</option>
+							<option value="1" <?if($pay_qiwi_agt == 1){?>selected<?}?>>Да</option>
+						</select>
+					</p>
+				</div>
+				<!-- END Qiwi -->
+				
+				<!-- WebMoney -->
+				<div class="grid_16">
+					<p>
+					<h2>Настройки Qiwi</h2>
+					</p>
+				</div>
+				<div class="grid_16">
+					<p>
+						<label>Кошелек <small>Номер кошелька для приема оплаты</small></label>
+						<input name="pay_wm_purse" value="<?=$pay_wm_purse?>" />
+					</p>
+				</div>
+				<!-- END WebMoney -->
+				
+				<div class="grid_16">
 					<p class="submit">
 						<input value="Сбросить" type="reset">
 						<input value="Сохранить" type="submit">
--- system/application/views/wdesigns/account/balance_r.php
+++ system/application/views/wdesigns/account/balance_r.php
@@ -1,3 +1,15 @@
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('input[name=payment_type]').change(function(){
+			type = $(this).val();
+			if(type == "qiwi")
+				$('#phone_block').css('display','block');
+			else
+				$('#phone_block').css('display','none');
+		});
+	});
+</script>
+
 <div id="yui-main">
 <div class="yui-b">
 
@@ -5,6 +17,17 @@
 <h1><a href="/account/applications">Баланс</a></h1>
 <p class="subtitle">Пополнение баланса.</p>
 
+<h2>Выберите способ оплаты</h2>
+<form action="">
+	<div>
+		<input type="radio" name="payment_type" id="payment_type_robox" value="robox" checked />&nbsp;<label for="payment_type_robox">РобоКасса</label>&nbsp;&nbsp;&nbsp;&nbsp;
+		<input type="radio" name="payment_type" id="payment_type_qiwi" value="qiwi" />&nbsp;<label for="payment_type_qiwi">Qiwi-кошелек</label>&nbsp;&nbsp;&nbsp;&nbsp;
+		<input type="radio" name="payment_type" id="payment_type_wm" value="wm" />&nbsp;<label for="payment_type_wm">WebMoney</label>&nbsp;&nbsp;&nbsp;&nbsp;
+		<input type="radio" name="payment_type" id="payment_type_yad" value="yad" />&nbsp;<label for="payment_type_yad">Яндекс.Деньги</label>
+	</div>
+	<div><label for="payment_sum">Сумма пополения</label>&nbsp;<input type="text" name="payment_sum" id="payment_sum" /> р.</div>
+	<div id="phone_block" style="display:none;"><label for="payment_phone">Номер телефона</label>&nbsp;<input type="text" name="payment_phone" id="payment_phone" /></div>
+</form>
 
 
 
